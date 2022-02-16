261a8df5e24cb34a85a58ff757dc5fa3d7011b60
shakhhhaa <tgbalpon@gmail.com>
2022-02-16 14:40:38 +0500

    sss

--- js/script.js
+++ js/script.js
@@ -61,3 +61,40 @@ function closeModal(modal) {
   modal.classList.remove('active')
   overlay.classList.remove('active')
 }
+
+
+
+
+$(document).ready(function () {
+  $('.submit').click(function (event) {
+    console.log('Clicked button')
+
+    var email = $('.email').val()
+    var subject = $('.subject').val()
+    var message = $('.message').val()
+    var statusElm = $('.status')
+    statusElm.empty()
+
+    if(email.length > 5 && email.includes('@') && email.includes('.')) {
+      statusElm.append('<div>Email is valid</div>')
+    }else{
+      event.preventDefault()
+      statusElm.append('<div>Email is not valid</div>')
+    }
+
+    if(subject.length >= 2) {
+      statusElm.append('<div>Email is valid</div>')
+    }else{
+      event.preventDefault()
+      statusElm.append('<div>Email is not valid</div>')
+    }
+
+    if(message.length >= 10){
+      statusElm.append('<div>Message is valid</div>')
+    }else{
+      event.preventDefault()
+      statusElm.append('<div>Message is not valid</div>')
+    }
+  })
+})
+
